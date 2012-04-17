285add7e44f4ae67918114422d598b0e9aee0990
Jan Paul Posma <jp.posma@gmail.com>
2012-04-17 13:37:36 +0100

    Added many methods and better highlighting

--- output/output.canvas.js
+++ output/output.canvas.js
@@ -50,9 +50,27 @@ module.exports = function(output) {
 			arcTo: {type: 'method', argsMin: 5, argsMax: 5, example: 'arcTo(20, 20, 100, 100, 60)', draws: false, mirror: true},
 			arc: {type: 'method', argsMin: 5, argsMax: 6, example: 'arc(100, 100, 30, 0, 360)', draws: false, mirror: true},
 			rect: {type: 'method', argsMin: 4, argsMax: 4, example: 'rect(100, 100, 100, 100)', draws: false, mirror: true},
+			scale: {type: 'method', argsMin: 2, argsMax: 2, example: 'scale(2.0, 3.0)', draws: true, mirror: true},
+			rotate: {type: 'method', argsMin: 1, argsMax: 1, example: 'rotate(0.40)', draws: true, mirror: true},
+			translate: {type: 'method', argsMin: 2, argsMax: 2, example: 'translate(10, 30)', draws: true, mirror: true},
+			transform: {type: 'method', argsMin: 6, argsMax: 6, example: 'transform(0.8, 0.3, 0.5, 1.0, 10, 30)', draws: true, mirror: true},
+			fillText: {type: 'method', argsMin: 3, argsMax: 4, example: 'fillText("Hello World!", 100, 100)', draws: true, mirror: true},
+			strokeText: {type: 'method', argsMin: 3, argsMax: 4, example: 'strokeText("Hello World!", 100, 100)', draws: true, mirror: true},
 			isPointInPath: {type: 'method', argsMin: 2, argsMax: 2, example: 'isPointInPath(150, 150)', draws: false, mirror: true},
 			fillStyle: {type: 'attribute', example: 'fillStyle = "#a00"', draws: false, mirror: false},
-			strokeStyle: {type: 'attribute', example: 'strokeStyle = "#a00"', draws: false, mirror: false}
+			strokeStyle: {type: 'attribute', example: 'strokeStyle = "#a00"', draws: false, mirror: false},
+			shadowOffsetX: {type: 'attribute', example: 'shadowOffsetX = 10', draws: false, mirror: true},
+			shadowOffsetY: {type: 'attribute', example: 'shadowOffsetY = 10', draws: false, mirror: true},
+			shadowBlur: {type: 'attribute', example: 'shadowBlur = 5', draws: false, mirror: false},
+			shadowColor: {type: 'attribute', example: 'shadowColor = "#3a3"', draws: false, mirror: false},
+			globalAlpha: {type: 'attribute', example: 'globalAlpha = 0.5', draws: false, mirror: false},
+			lineWidth: {type: 'attribute', example: 'lineWidth = 3', draws: false, mirror: false},
+			lineCap: {type: 'attribute', example: 'lineCap = "round"', draws: false, mirror: true},
+			lineJoin: {type: 'attribute', example: 'lineJoin = "bevel"', draws: false, mirror: true},
+			miterLimit: {type: 'attribute', example: 'miterLimit = 3', draws: false, mirror: true},
+			font: {type: 'attribute', example: 'font = "40pt Calibri"', draws: false, mirror: true},
+			textAlign: {type: 'attribute', example: 'textAlign = "center"', draws: false, mirror: true},
+			textBaseline: {type: 'attribute', example: 'textBaseline = "top"', draws: false, mirror: true}
 		},
 
 		getAugmentedObject: function() {
@@ -80,8 +98,8 @@ module.exports = function(output) {
 		},
 
 		handleMethod: function(node, name, args) {
-			if (this.highlighting) this.highlight(node, name, args);
-			return this.context[name].apply(this.context, args);
+			if (this.highlighting) return this.highlight(node, name, args);
+			else return this.context[name].apply(this.context, args);
 		},
 
 		handleAttributeGet: function(node, name) {
@@ -91,23 +109,25 @@ module.exports = function(output) {
 		handleAttributeSet: function(node, name, value) {
 			// for now there are no attributes that can cause a highlight
 			this.context[name] = value;
-			if (name === 'strokeStyle') {
-				this.actualStrokeStyle = this.context.strokeStyle;
-			} else {
-				this.actualFillStyle = this.context.fillStyle;
+			if (this.highlighting) {
+				if (this.functions[name].mirror) this.mirrorContext[name] = value;
+
+				if (name === 'strokeStyle') {
+					this.actualStrokeStyle = this.context.strokeStyle;
+				} else if (name === 'fillStyle') {
+					this.actualFillStyle = this.context.fillStyle;
+				} else if (name === 'shadowColor') {
+					this.actualShadowColor = this.context.shadowColor;
+				}
 			}
 		},
 
 		startHighlighting: function() {
 			this.highlightNextShapes = true;
-			this.actualStrokeStyle = this.context.strokeStyle;
-			this.actualFillStyle = this.context.fillStyle;
 		},
 
 		stopHighlighting: function() {
 			this.highlightNextShapes = false;
-			this.context.strokeStyle = this.actualStrokeStyle;
-			this.context.fillStyle = this.actualFillStyle;
 		},
 
 		enableHighlighting: function() {
@@ -146,27 +166,46 @@ module.exports = function(output) {
 				this.mirrorContext.clearRect(0, 0, this.size, this.size);
 				this.mirrorContext.beginPath();
 				this.highlightCallCounter = 1;
+				this.actualStrokeStyle = this.context.strokeStyle;
+				this.actualFillStyle = this.context.fillStyle;
+				this.actualShadowColor = this.context.shadowColor;
 			}
 		},
 
 		/// INTERNAL FUNCTIONS ///
 		highlight: function(node, name, args) {
 			if (this.functions[name].draws) {
-				// some spread is needed between the numbers as borders are blurred, and colour information is thus not 100% reliable
-				// therefore we use calculation modulo prime, so that eventually all numbers are used, and this also introduces a nice cycle,
-				// so that colours can be used again; the assumption is that whenever there are so many elements on the screen, the ones
-				// that introduced faulty colours, or the original ones in case of reusing colours, are most likely overwritten already
-				this.highlightCallCounter = (this.highlightCallCounter + 464651) % 16777213;
-				var color = 'rgba(' + (~~(this.highlightCallCounter/65536)%256) + ',' + (~~(this.highlightCallCounter/256)%256) + ',' + (this.highlightCallCounter%256) + ', 1)';
-				this.mirrorContext.strokeStyle = color;
-				this.mirrorContext.fillStyle = color;
+				if (this.functions[name].mirror && this.context.globalAlpha > 0) {
+					// some spread is needed between the numbers as borders are blurred, and colour information is thus not 100% reliable
+					// therefore we use calculation modulo prime, so that eventually all numbers are used, and this also introduces a nice cycle,
+					// so that colours can be used again; the assumption is that whenever there are so many elements on the screen, the ones
+					// that introduced faulty colours, or the original ones in case of reusing colours, are most likely overwritten already
+					this.highlightCallCounter = (this.highlightCallCounter + 464651) % 16777213;
+					//this.highlightCallCounter++;
+					var color = 'rgba(' + (~~(this.highlightCallCounter/65536)%256) + ',' + (~~(this.highlightCallCounter/256)%256) + ',' + (this.highlightCallCounter%256) + ', 1)';
+					this.mirrorContext.strokeStyle = color;
+					this.mirrorContext.fillStyle = color;
+					this.mirrorContext.shadowColor = color;
+					this.mirrorContext.lineWidth = Math.max(3, this.context.lineWidth);
+					this.mirrorContext[name].apply(this.mirrorContext, args);
+				}
+
+				if (this.highlightNextShapes || this.highlightCallCounter === this.highlightCallTarget) {
+					this.context.strokeStyle = 'rgba(5, 195, 5, 0.85)';
+					this.context.fillStyle = 'rgba(5, 195, 5, 0.85)';
+					this.context.shadowColor = 'rgba(5, 195, 5, 0.85)';
+					if (this.highlightCallTarget > 0) this.editor.highlightNode(node);
+
+					var retVal = this.context[name].apply(this.context, args);
+					this.context.strokeStyle = this.actualStrokeStyle;
+					this.context.fillStyle = this.actualFillStyle;
+					this.context.shadowColor = this.actualShadowColor;
+					return retVal;
+				}
+			} else {
+				if (this.functions[name].mirror) this.mirrorContext[name].apply(this.mirrorContext, args);
 			}
-			if (this.functions[name].mirror) this.mirrorContext[name].apply(this.mirrorContext, args);
-			if (this.highlightNextShapes || this.highlightCallCounter === this.highlightCallTarget) {
-				this.context.strokeStyle = 'rgba(5, 195, 5, 0.85)';
-				this.context.fillStyle = 'rgba(5, 195, 5, 0.85)';
-				if (this.highlightCallTarget > 0) this.editor.highlightNode(node);
-			}
+			return this.context[name].apply(this.context, args);
 		},
 
 		mouseMove: function(event) {
@@ -176,7 +215,7 @@ module.exports = function(output) {
 				var pixel = this.mirrorContext.getImageData(x, y, 1, 1).data;
 				// use the alpha channel as an extra safeguard
 				var target = (pixel[3] < 255 ? 0 : (pixel[0]*65536 + pixel[1]*256 + pixel[2]) % 16777213);
-				console.log(pixel, target);
+				//console.log(pixel, target);
 
 				if (this.highlightCallTarget !== target) {
 					this.highlightCallTarget = target;
