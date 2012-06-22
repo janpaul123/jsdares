800ff9fe74511a2c4440de35a5e30d53d95fbb93
Jan Paul Posma <jp.posma@gmail.com>
2012-06-22 13:38:55 +0100

    Better error messages

--- jsmm/jsmm.parser.js
+++ jsmm/jsmm.parser.js
@@ -36,35 +36,66 @@ module.exports = function(jsmm) {
 			}
 		};
 		
-		//console.log(hash.text);
-		var suggestionError = function(suggestion) {
-			throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered' + makeNear(hash.text) + ', perhaps there is a <var>' + suggestion + '</var> missing', errStr);
+		var suggestionError = function(suggestion, an) {
+			throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered' + makeNear(hash.text) + ', perhaps there is ' + (an ? 'an' : 'a') +' <var>' + suggestion + '</var> missing', errStr);
 		};
-		
-		if (token === "RESERVED") {
+
+		if (token === 'RESERVED') {
 			// special case: passing on the information that the word is reserved
 			throw new jsmm.msg.CriticalError(loc, 'Unfortunately <var>' + hash.text + '</var> is a reserved word, which means you cannot use it as a variable name', errStr);
+		} else if (token === 'INVALID') {
+			// special cases: passing on information about the used symbol
+			if (hash.text === "'") {
+				throw new jsmm.msg.CriticalError(loc, 'Unfortunately <var>\'</var> cannot be used, please use double quotes (<var>"</var>) instead', errStr);
+			} else if (['<<', '>>', '>>=', '<<='].indexOf(hash.text) >= 0) {
+				throw new jsmm.msg.CriticalError(loc, 'Unfortunately the bit shift operator <var>' + hash.text + '</var> cannot be used', errStr);
+			} else if (['===', '!=='].indexOf(hash.text) >= 0) {
+				throw new jsmm.msg.CriticalError(loc, 'Unfortunately the strict equality operator <var>' + hash.text + '</var> cannot be used', errStr);
+			} else if (['?', ':', '\\'].indexOf(hash.text) >= 0) {
+				throw new jsmm.msg.CriticalError(loc, 'Unfortunately the symbol <var>' + hash.text + '</var> cannot be used', errStr);
+			} else if (hash.text === '$') {
+				throw new jsmm.msg.CriticalError(loc, 'Unfortunately the symbol <var>$</var>, often used for the jQuery library, cannot be used', errStr);
+			}
+		} else if (token === '"') {
+			throw new jsmm.msg.CriticalError(loc, 'This string has not been closed, please add another <var>"</var> on this line', errStr);
 		} else if (hash.token === null) {
 			// lexer error
 			loc = {line: hash.line+1, column: 0};
 			throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered', errStr);
 		} else if (expected.length === 1 && expected[0] === 'NEWLINE') {
-			throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered, perhaps some code' + makeNear(hash.text) + ' should be put on a new line.', errStr);
+			if (token === 'ELSE') {
+				// two or more else branches gives this situation
+				throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered, perhaps the <var>else</var> branches are incorrect', errStr);
+			} else if (token === 'IF') {
+				// "else if" without the "else" gives this
+				suggestionError('else', true);
+			} else {
+				throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered, perhaps some code' + makeNear(hash.text) + ' should be put on a new line', errStr);
+			}
 		} else if (expected.length === 1) {
 			// if only one thing can be expected, pass it on
 			if (expected[0] === 'NAME') {
 				expected[0] = 'variable name';
 			}
 			suggestionError(expected[0]);
-		} else if (expected.indexOf(";") >= 0 && token === "NEWLINE") {
+		} else if (expected.indexOf(';') >= 0 && token === 'NEWLINE') {
 			// ; expected before of newline is usually forgotten
 			suggestionError(';');
-		} else if (expected.indexOf("}") >= 0 && ["FUNCTION", "EOF"].indexOf(token) >= 0) {
-			// } expected before function declaration or eof is usually forgotten
+		} else if (expected.indexOf('==') >= 0 && token === '=') {
+			// ; expected before of newline is usually forgotten
+			throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered' + makeNear(hash.text) + ', perhaps it should be <var>==</var> instead', errStr);
+		} else if (expected.indexOf('}') >= 0 && ['FUNCTION', 'ELSE', 'EOF'].indexOf(token) >= 0) {
+			// } expected before function declaration, else, eof is usually forgotten
 			suggestionError('}');
-		} else if (expected.indexOf(")") >= 0 && ["{", ";", "NEWLINE"].indexOf(token) >= 0) {
+		} else if (expected.indexOf(')') >= 0 && ['{', ';', 'NEWLINE'].indexOf(token) >= 0) {
 			// ) expected before { or ; is usually forgotten
 			suggestionError(')');
+		} else if (expected.indexOf('TRUE') >= 0 && token === '{') {
+			// expression expected, but { given, usually someone tries to make an object
+			throw new jsmm.msg.CriticalError(loc, 'You cannot use <var>{</var> here, only after <var>if</var>, <var>else</var>, <var>while</var>, <var>for</var>, and <var>function</var>', errStr);
+		} else if (expected.indexOf('TRUE') >= 0 && token === 'FUNCTION') {
+			// expression expected, but "FUNCTION" given, usually someone tries to make an inline function
+			throw new jsmm.msg.CriticalError(loc, 'You cannot use <var>function</var> here, only on a separate line', errStr);
 		} else {
 			throw new jsmm.msg.CriticalError(loc, 'Invalid syntax encountered' + makeNear(hash.text), errStr);
 		}
--- jsmm/jsmmparser.jison
+++ jsmm/jsmmparser.jison
@@ -16,11 +16,13 @@ fraction		(?:"."{digit}+)
 number			(?:(?:(?:[1-9]{digit}*)|"0"){fraction}?{exponent}?)
 string			(?:["][^\\"\n]*(?:[\\][nt"\\][^\\"\n]*)*["])
 reserved		(?:"null"|"break"|"case"|"catch"|"default"|"finally"|"instanceof"|"new"|"continue"|"void"|"delete"|"this"|"do"|"in"|"switch"|"throw"|"try"|"typeof"|"with"|"abstract"|"boolean"|"byte"|"char"|"class"|"const"|"debugger"|"double"|"enum"|"export"|"extends"|"final"|"float"|"goto"|"implements"|"import"|"int"|"interface"|"long"|"native"|"package"|"private"|"protected"|"public"|"short"|"static"|"super"|"synchronized"|"throws"|"transient"|"volatile"|"arguments"|"NaN"|"Array"|"Object"|"RegExp"|"toString"|(?:"jsmm"{alphanum}*))
+invalid			(?:"'"|">>="|"<<="|">>"|"<<"|"==="|"!=="|"?"|":"|"$"|"\\")
 
 %%
 
 (?:{whitespace}|{linecomment})				/* skip and comments */
 {reserved}(?!{alphanum})					return "RESERVED";
+{invalid}									return "INVALID";
 "true"										return "TRUE";
 "false"										return "FALSE";
 "if"										return "IF";
@@ -52,6 +54,7 @@ reserved		(?:"null"|"break"|"case"|"catch"|"default"|"finally"|"instanceof"|"new
 "]"											return "]";
 "."											return ".";
 ","											return ",";
+"\""										return '"';
 
 /lex
 
