1ea0f0c9d392f941f818ed5e6a031369f71d7c0a
Jan Paul Posma <jp.posma@gmail.com>
2012-08-02 01:36:56 +0100

    Made header clickable

--- client/client.init.js
+++ client/client.init.js
@@ -4,5 +4,10 @@
 module.exports = function(client) {
 	client.init = function() {
 		var manager = new client.Manager($('#content'));
+
+		$('#header-title').on('click', function(event) {
+			event.preventDefault();
+			manager.navigateTo('/');
+		});
 	};
 };
