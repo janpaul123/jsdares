e8bf0f3aa6d0e8ce8d44c35fe1e3bf0c5b930ccd
Jan Paul Posma <me@janpaulposma.nl>
2013-04-30 16:15:55 +0200

    Fixed path in server-entry

--- src/server-entry.js
+++ src/server-entry.js
@@ -3,4 +3,4 @@
 
 process.env.JSDARES_PORT = process.env.JSDARES_PORT || 3000;
 
-require('./server-connect').listen(process.env.JSDARES_PORT);
+require('./server-connect-grunt').listen(process.env.JSDARES_PORT);
